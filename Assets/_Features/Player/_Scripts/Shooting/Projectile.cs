b0711a019847abc8242d12b15280ad4586fbd7a3
dimitrimm <dimitrimarinovic@gmail.com>
2025-02-17 19:17:13 -0300

    fix: merge conflit resol

--- Assets/_Features/Player/_Scripts/Shooting/Projectile.cs
+++ Assets/_Features/Player/_Scripts/Shooting/Projectile.cs
@@ -19,17 +19,24 @@ namespace TopDown.Shooting
         public void ShootBullet(Vector2 direction)
         {
             lifeTimer = 0;
-            body.linearVelocity = Vector2.zero;
             transform.up = direction; // Ajusta a rotação do projétil para a direção do tiro
             gameObject.SetActive(true);
 
-            body.linearVelocity = direction * speed; // Define a velocidade constante do projétil
+            body.linearVelocity = direction.normalized * speed; // Define a velocidade constante do projétil
         }
 
         private void Update()
         {
             lifeTimer += Time.deltaTime;
             if (lifeTimer >= lifeTime)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Obstacle"))
             {
                 Destroy(gameObject);
             }
