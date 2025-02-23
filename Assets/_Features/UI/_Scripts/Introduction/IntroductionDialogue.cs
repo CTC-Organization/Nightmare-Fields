416e8a3f83f4359fa000babbdd6708f69416af0c
LuizFprog <luizgomes.prog@gmail.com>
2025-02-23 10:27:39 -0300

    fix: updated main menu visual

--- Assets/_Features/UI/_Scripts/Introduction/IntroductionDialogue.cs
+++ Assets/_Features/UI/_Scripts/Introduction/IntroductionDialogue.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class IntroductionDialogue : MonoBehaviour
 {
     public GameObject dialoguePanel;
-    public Text dialogueText;
+    public TextMeshProUGUI dialogueText;
     public string[] dialogue;
     private int index;
 
@@ -60,7 +60,7 @@ public class IntroductionDialogue : MonoBehaviour
         }
         else
         {
-            SceneManager.LoadScene("TESTEInventarioDialogo");
+            SceneManager.LoadScene(nextSceneName);
         }
     }
 }
